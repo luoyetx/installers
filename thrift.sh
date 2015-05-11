632cd259138d862f35eb3b50bc64e4040b78fcc1
zhangjie <luoyetx@gmail.com>
2015-05-11 05:30:41 -0400

    fix url

--- thrift.sh
+++ thrift.sh
@@ -9,7 +9,7 @@ VERSION=0.9.2
 yum install -y libevent-devel zlib-devel openssl-devel
 
 # fetch source code
-wget -c http://mirrors.hust.edu.cn/apache/thrift/${VERSION}/thrift-${VERSION}.tar.gz
+wget -c http://apache.fayea.com/thrift/${VERSION}/thrift-${VERSION}.tar.gz
 tar -zxvf thrift-${VERSION}.tar.gz
 
 # make
