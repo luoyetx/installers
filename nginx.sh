90c4ffc22029746baebf9e80f592dcd9aa8e99c3
zhangjie <luoyetx@gmail.com>
2015-05-11 13:18:08 +0800

    add nginx
    nginx-1.8.0

---
+++ nginx.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+. BasicConfig.sh
+
+# configure
+VERSION=1.8.0
+
+# install dependencies
+yum install -y pcre-devel libxslt-devel gd-devel perl-devel perl-ExtUtils-Embed GeoIP-devel
+
+# fetch source code
+wget -c http://nginx.org/download/nginx-${VERSION}.tar.gz
+tar -zxvf nginx-${VERSION}.tar.gz
+
+# make
+cd nginx-${VERSION}
+# configure parameters copirf from CentOS
+./configure --prefix=/usr/share/nginx --sbin-path=/usr/sbin/nginx --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error.log --http-log-path=/var/log/nginx/access.log --http-client-body-temp-path=/var/lib/nginx/tmp/client_body --http-proxy-temp-path=/var/lib/nginx/tmp/proxy --http-fastcgi-temp-path=/var/lib/nginx/tmp/fastcgi --http-uwsgi-temp-path=/var/lib/nginx/tmp/uwsgi --http-scgi-temp-path=/var/lib/nginx/tmp/scgi --pid-path=/var/run/nginx.pid --lock-path=/var/lock/subsys/nginx --user=nginx --group=nginx --with-file-aio --with-ipv6 --with-http_ssl_module --with-http_realip_module --with-http_addition_module --with-http_xslt_module --with-http_image_filter_module --with-http_geoip_module --with-http_sub_module --with-http_dav_module --with-http_flv_module --with-http_mp4_module --with-http_gzip_static_module --with-http_random_index_module --with-http_secure_link_module --with-http_degradation_module --with-http_stub_status_module --with-http_perl_module --with-mail --with-mail_ssl_module --with-debug
+make -j${CONCURRENCY}
+
+# install
+make install
