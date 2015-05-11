cf3219c9076911a45d1e14d4c2ef0039964f9387
zhangjie <luoyetx@gmail.com>
2015-05-11 06:39:53 -0400

    add python support

--- opencv.sh
+++ opencv.sh
@@ -3,7 +3,8 @@
 . BasicConfig.sh
 
 # configure
-VERSION=2.4.10
+VERSION=2.4.9
+PYTHON_ROOT=/usr/local
 
 # install dependencies
 
@@ -13,7 +14,16 @@ unzip opencv-${VERSION}
 
 # make
 cd opencv-${VERSION}
-cmake -DCMAKE_BUILD_TYPE=RELEASE .
+cmake . -DCMAKE_BUILD_TYPE=RELEASE
+# uncomment to add python support
+#cmake . -DCMAKE_BUILD_TYPE=RELEASE \
+#-DBUILD_NEW_PYTHON_SUPPORT=ON \
+#-DPYTHON_EXECUTABLE=${PYTHON_ROOT}/bin/python2.7 \
+#-DPYTHON_INCLUDE_DIR=${PYTHON_ROOT}/include/python2.7/ \
+#-DPYTHON_LIBRARY=${PYTHON_ROOT}/lib/libpython2.7.so \
+#-DPYTHON_NUMPY_INCLUDE_DIR=${PYTHON_ROOT}/lib/python2.7/site-packages/numpy/core/include \
+#-DPYTHON_PACKAGES_PATH=${PYTHON_ROOT}/lib/python2.7/site-packages/ \
+#-DBUILD_PYTHON_SUPPORT=ON
 make -j${CONCURRENCY}
 
 # install
